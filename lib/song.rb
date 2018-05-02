be0fe5e5a057f7d2555b85a5087bca4d86b69d75
Eric DeSimone <eric.desimone@cortland.edu>
2018-05-02 14:15:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,8 @@ class Song
     title = title.split("- ")
     @artist_name = title[0]
     @name = title[1]
-    @name.chomp(". ")!
+    @name.chomp(". ")
+    @name
     song = self.new
     song.name = @name
     song.artist_name = @artist_name
